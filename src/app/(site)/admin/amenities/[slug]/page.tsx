848033b567864d9f091be28a44b132a792e41a78
daluicio86 <diego.aluicio@gmail.com>
2025-11-19 08:38:12 -0500

    d36

--- src/app/(site)/admin/amenities/[slug]/page.tsx
+++ src/app/(site)/admin/amenities/[slug]/page.tsx
@@ -3,22 +3,16 @@ import HeroSub from '@/components/shared/HeroSub';
 import { getAmenitieByName } from '@/actions/amenitie/get-amenitie-by-name';
 import { AmenitieForm } from './ui/AmenitieForm';
 
-type Props = {
-  searchParams?: {
-    slug?: string;
-  };
-};
-
 export default async function AmenitiesPage({
   params,
 }: {
-  params: Promise<{ slug: string }>
+  params: Promise<{ slug?: string }>
 }) {
   const { slug } = await params;
 
   console.log('Slug recibido:', slug);
 
-  const amenitie = await getAmenitieByName(slug);
+const amenitie = await getAmenitieByName(slug ?? '');
 
   // Si usas "new" como modo creación:
   /*
