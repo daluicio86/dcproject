7977aea2726ad8b205a27de5801a86fafa67071b
daluicio86 <diego.aluicio@gmail.com>
2025-11-19 10:57:51 -0500

    d42

--- src/app/(site)/admin/amenities/page.tsx
+++ src/app/(site)/admin/amenities/page.tsx
@@ -4,15 +4,22 @@ import { Metadata } from "next";
 import { Pagination } from "@/components/Tables/Pagination";
 import { getPaginatedAmenities } from "@/actions/amenitie/amenitie-pagination";
 import AmenitiesListing from "@/components/Amenities/AmenitieList";
+
 export const metadata: Metadata = {
     title: "Lista de servicios| Buying and selling",
 };
 
-export default async function AmenitiesPage({ params, }: { params: { page?: string } }) {
-    const { page } =  params;
+interface Props {
+    searchParams: Promise<{
+        page?: string;
+    }>;
+}
+
+export default async function AmenitiesPage({ searchParams }: Props) {
+    const { page } = await searchParams;
     const pageNumber = page ? parseInt(page) : 1;
 
-    const { amenities, /*currentPage, */totalPages } = await getPaginatedAmenities({ page: pageNumber });
+    const { amenities, totalPages } = await getPaginatedAmenities({ page: pageNumber });
 
     return (
         <>
@@ -26,4 +33,3 @@ export default async function AmenitiesPage({ params, }: { params: { page?: stri
         </>
     );
 };
-
