3dec7fbf7a93e223191321ec78db19d443ac9186
daluicio86 <diego.aluicio@gmail.com>
2025-11-20 08:22:34 -0500

    d60

---
+++ src/app/api/auth/[...nextauth]/auth.config.ts
@@ -0,0 +1,80 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+import { signInEmailPassword } from "@/auth/actions/auth-actions";
+import { Adapter } from "next-auth/adapters";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      authorize: async (credentials, req) => {
+        const user = await signInEmailPassword(
+          credentials!.username,
+          credentials!.password
+        );
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  session: { strategy: "jwt" },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = (user as any).role ?? "user";
+        token.isActive = (user as any).isActive ?? true;
+      } else {
+        const dbUser = await prisma.user.findUnique({
+          where: {
+            email: token.email ?? undefined,
+          },
+        });
+
+        if (dbUser) {
+          token.id = dbUser.id;
+          token.role = dbUser.role;
+          token.isActive = dbUser.isActive;
+        } else {
+          token.id = token.id ?? "";
+          token.role = token.role ?? "user";
+          token.isActive = token.isActive ?? true;
+        }
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      if (session && session.user) {
+        session.user.id = token.id as string;
+        session.user.role = token.role as string;
+        session.user.isActive = token.isActive as boolean;
+      }
+      return session;
+    },
+  },
+};
