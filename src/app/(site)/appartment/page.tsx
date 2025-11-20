f3ed019763e148b8e8163abaa2b10ea1691faafd
daluicio86 <diego.aluicio@gmail.com>
2025-11-20 09:09:21 -0500

    d68

--- src/app/(site)/appartment/page.tsx
+++ src/app/(site)/appartment/page.tsx
@@ -14,7 +14,7 @@ const page = () => {
                 description="Experience elegance and comfort with our exclusive luxury  villas, designed for sophisticated living."
                 badge="Properties"
             />
-            <Appartment />
+            <Appartment items={[]} />
         </>
     );
 };
