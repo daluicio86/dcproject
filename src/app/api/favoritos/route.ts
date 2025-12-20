6e01ec599a03dd4171aa704c6febc1eafadc37f6
daluicio86 <diego.aluicio@gmail.com>
2025-12-20 06:59:54 -0500

    d137

--- src/app/api/favoritos/route.ts
+++ src/app/api/favoritos/route.ts
@@ -2,7 +2,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/auth.config";
 
 import { NextResponse } from "next/server";
-import { prisma } from "../../../../lib/prisma";
+import prisma from "@/lib/prisma";
+
 
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
